95b9db6a7d99ff393a0b2ac9bc7553f2b80f253c
audyyla <afrilicia.audila@gmail.com>
2026-01-03 19:36:43 +0700

    sll.cpp

--- sll.cpp
+++ sll.cpp
@@ -7,25 +7,31 @@ void createList(list &L) {
 }
 
 address createNewElm(infotype x) {
-    address p = new elmList;
+    address p;
+
+    p = new elmList;
     p->info = x;
     p->next = nullptr;
     return p;
 }
 
 void tambahData(list &L, infotype x) {
-    address p = createNewElm(x);
+    address p;
+
+    p = createNewElm(x);
     p->next = L.first;
     L.first = p;
 }
 
 address panenTerbanyak(list L) {
+    address max, p;
+
     if (L.first == nullptr) {
         return nullptr;
     }
 
-    address max = L.first;
-    address p = L.first->next;
+    max = L.first;
+    p = L.first->next;
 
     while (p != nullptr) {
         if (p->info.jumlah > max->info.jumlah) {
@@ -37,7 +43,8 @@ address panenTerbanyak(list L) {
 }
 
 void panenBulanan(list L, string bulan) {
-    address p = L.first;
+    address p;
+    p = L.first;
     while (p != nullptr) {
         if (p->info.bulan == bulan) {
             cout << p->info.namaTanaman << " ";
