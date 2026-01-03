f990e502525ab9992683cb2bcc197e72c6f807c5
audyyla <afrilicia.audila@gmail.com>
2026-01-03 19:16:40 +0700

    Create sll.cpp

---
+++ sll.cpp
@@ -0,0 +1,48 @@
+#include "sll.h"
+#include <iostream>
+using namespace std;
+
+void createList(list &L) {
+    L.first = nullptr;
+}
+
+address createNewElm(infotype x) {
+    address p = new elmList;
+    p->info = x;
+    p->next = nullptr;
+    return p;
+}
+
+void tambahData(list &L, infotype x) {
+    address p = createNewElm(x);
+    p->next = L.first;
+    L.first = p;
+}
+
+address panenTerbanyak(list L) {
+    if (L.first == nullptr) {
+        return nullptr;
+    }
+
+    address max = L.first;
+    address p = L.first->next;
+
+    while (p != nullptr) {
+        if (p->info.jumlah > max->info.jumlah) {
+            max = p;
+        }
+        p = p->next;
+    }
+    return max;
+}
+
+void panenBulanan(list L, string bulan) {
+    address p = L.first;
+    while (p != nullptr) {
+        if (p->info.bulan == bulan) {
+            cout << p->info.namaTanaman << " ";
+        }
+        p = p->next;
+    }
+    cout << endl;
+}
